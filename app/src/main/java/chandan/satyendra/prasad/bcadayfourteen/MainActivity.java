496772d8ebd48e17f76d8b6ec6dfc91311b96b52
ChandanSatyendraPrasad <chandansatyendraprasad@gmail.com>
2022-03-29 14:29:09 +0530

    Initial commit

---
+++ app/src/main/java/chandan/satyendra/prasad/bcadayfourteen/MainActivity.java
@@ -0,0 +1,48 @@
+package chandan.satyendra.prasad.bcadayfourteen;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+    ImageView imgView;
+    Button btnTakeImage;
+    final int CAMERA_REQUEST = 7080;
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == CAMERA_REQUEST && resultCode == Activity.RESULT_OK) {
+            assert data != null;
+            Bitmap photo = (Bitmap) data.getExtras().get("data");
+            imgView.setImageBitmap(photo);
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        imgView = findViewById(R.id.img_view);
+        btnTakeImage = findViewById(R.id.btn_image);
+
+        btnTakeImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(cameraIntent, CAMERA_REQUEST);
+            }
+        });
+
+    }
+}
